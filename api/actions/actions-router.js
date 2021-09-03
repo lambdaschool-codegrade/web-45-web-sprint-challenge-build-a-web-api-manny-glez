32bb46c18762050cf17f1161b0740622f5e54761
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-09-03 18:28:59 -0500

    adds endpoints

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,88 @@
-// Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model');
+const router = express.Router();
+
+// [GET] /api/actions
+router.get('/', (req, res) => {
+  Action.get(req.query)
+    .then(actions => {
+      res.status(200).json(actions)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        messgae: 'Error getting actions'
+      })
+    })
+})
+
+// [GET] /api/actions/:id
+router.get('/:id', (req, res) => {
+  Action.get(req.params.id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action)
+      } else {
+        res.status(404).json({ messgae: 'action not found' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        messgae: 'Error getting action'
+      })
+    })
+})
+
+// [POST] /api/actions
+router.post('/', (req, res) => {
+  Action.insert(req.body)
+    .then(action => {
+      res.status(201).json(action)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        messgae: 'error adding action'
+      })
+    })
+})
+
+// [PUT] /api/actions/:id
+router.put('/:id', (req, res) => {
+  const changes = req.body
+  Action.update(req.params.id, changes)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action)
+      } else {
+        res.status(404).json({ messgae: 'action not found' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        messgae: 'error updating'
+      })
+    })
+})
+
+// [DELETE] /api/actions/:id
+router.delete('/:id', (req, res) => {
+  Action.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ messgae: 'deleted' })
+      } else {
+        res.status(404).json({ messgae: 'not found' })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        messgae: 'error removing'
+      })
+    })
+})
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,96 @@
-// Write your "projects" router here!
+const express = require('express');
+const Project = require('./projects-model');
+const router = express.Router();
+
+// [GET] /api/projects
+router.get('/', async (req, res) => {
+  try {
+    const data = await Project.get()
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json([])
+  }
+})
+
+// [GET] /api/projects/:id
+router.get('/:id', (req, res) => {
+  Project.get(req.params.id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project)
+      } else {
+        res.status(404).json({ message: 'project not found' })
+      }
+    })
+})
+
+// [POST] /api/projects
+router.post('/', (req, res) => {
+  Project.insert(req.body)
+    .then(project => {
+      res.status(201).json(project)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(400).json({
+        message: 'Error adding the project'
+      })
+    })
+})
+
+// [PUT] /api/projects/:id
+router.put(':id', (req, res) => {
+  const changes = req.body;
+  Project.update(req.params.id, changes)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project)
+      } else {
+        res.status(404).json({ message: 'could not find project' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).json({
+        message: 'error updating'
+      })
+    })
+})
+
+// [DELETE] /api/projects/:id
+router.delete('/:id', (req, res) => {
+  Project.remove(req.params.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'the project has been deleted' })
+      } else {
+        res.status(404).json({ message: 'no project found' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'Error removing project'
+      })
+    })
+})
+
+// [GET] /api/projects/:id/actions
+router.get('/:id/actions', (req, res) => {
+  Project.getProjectActions(req.params.id)
+    .then(actions => {
+      if (actions.length > 0) {
+        res.status(200).json(actions)
+      } else {
+        res.status(404).json({ message: 'not found' })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({
+        message: 'error retrieving'
+      })
+    })
+})
+
+module.exports = router
